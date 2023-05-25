dc65cfc2a8d81c903e637259491903e08f32d533
Tumanbaev Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-25 05:50:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
- driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
